090063f040803e0f619e2eed7f4a66a9db1c7f81
unknown <you@example.com>
2022-05-10 15:50:18 -0500

    actualización de la base de datos

--- BD/storecollector.sql
+++ BD/storecollector.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `storecollector` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `storecollector`;
 -- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
 --
 -- Host: localhost    Database: storecollector
@@ -77,10 +79,10 @@ DROP TABLE IF EXISTS `objeto`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `objeto` (
   `id_objeto` int NOT NULL,
-  `nombre_objeto` varchar(50) NOT NULL,
-  `descripcion` varchar(50) NOT NULL,
+  `nombre_objeto` varchar(100) NOT NULL,
+  `descripcion` varchar(200) NOT NULL,
   `estado` int NOT NULL,
-  `precio` int NOT NULL,
+  `precio` float NOT NULL,
   `contacto` varchar(250) DEFAULT NULL,
   `nombre_imagen` varchar(100) DEFAULT NULL,
   `id_usuario` int NOT NULL,
@@ -99,7 +101,7 @@ CREATE TABLE `objeto` (
 
 LOCK TABLES `objeto` WRITE;
 /*!40000 ALTER TABLE `objeto` DISABLE KEYS */;
-INSERT INTO `objeto` VALUES (1,'Pretty Violent #1','Comic Pretty Violent #1 firmado por Ryan Ottley',10,250,NULL,NULL,1,1),(2,'Spider-Man 2099: Exodus Alpha #1','Comic del Hombre araña 2099, del año 2022\nDe Steve',10,1100,'correo electronico: qwerty@hotmail.com\nInstagram: @comics_chima',NULL,2,1),(3,'Grogu-the mandalorian','Figura Funko pop de Grogu con una bola',10,600,'Instagram: @tienditadefunkos\ncorreo eléctronico: tienditadefunkos@gmail.com',NULL,3,2),(4,'black panther ','Prueba de creacion',8,150,'telefono: 5656545654','1652056756796.jpeg',1,1);
+INSERT INTO `objeto` VALUES (1,'Pretty Violent #1','Comic Pretty Violent #1 firmado por Ryan Ottley',10,250,NULL,NULL,1,1),(2,'Spider-Man 2099: Exodus Alpha #1','Comic del Hombre araña 2099, del año 2022\nDe Steve',10,1100,'correo electronico: qwerty@hotmail.com\nInstagram: @comics_chima',NULL,2,1),(3,'Grogu-the mandalorian','Figura Funko pop de Grogu con una bola',10,600,'Instagram: @tienditadefunkos\ncorreo eléctronico: tienditadefunkos@gmail.com',NULL,3,2),(4,'black panther ','Prueba de creacion',8,150,'telefono: 5656545654','1652056756796.jpeg',1,1),(5,'The killing joke','Comic hecho por Dennis O\'Neil, primera edicion',9,1500,'por correo electronico: iahjkah@gmail.com','1652208982918.jpeg',3,1),(6,'The killing joke','Comic hecho por Dennis O\'Neil, primera edicion',9,1500,'por correo electronico: iahjkah@gmail.com','1652209059196.jpeg',3,1),(7,'Batman who laughs','comic en buenas condiciones',8,100,'instagram','1652209235737.jpeg',3,1),(8,'Thor: renacido','comic creado por  J. Michael Straczynski (Autor)',10,1500,'Pagina de facebook: comics store\r\nInstagram: @comicStore_chimal','1652209904716.jpeg',1,1),(9,'Thor who holds the hammer?','Por Jason Aaron',6,340,'Enlace de venta: https://www.amazon.com.mx/Thor-2-Who-Holds-Hammer/dp/0785197850/ref=asc_df_0785197850/?tag=gledskshopmx-20&linkCode=df0&hvadid=329497570229&hvpos=&hvnetw=g&hvrand=244058303485681820&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&','1652210170175.jpeg',4,1);
 /*!40000 ALTER TABLE `objeto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -126,7 +128,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'qwerty','qwerty@hotmail.com','qwerty','qwerty'),(2,'Juan Perez','JuanPerez@hotmail.com','JP','JP'),(3,'Jesus Maximiliano Tostado Navarro','pruebaCorreo@gmail.com','JM_tost','prueba1231456_'),(4,'ojaoijdfdsjfd','perezJ@gmail.com','sjdksdjfh_15','iguhuhiu789_'),(5,'aakjhdajskdh ajkhsjdak dh','akjshdaksj@gmail.com','sksjhf_12','isahsjhjksdfh');
+INSERT INTO `usuario` VALUES (1,'qwerty','qwerty@hotmail.com','qwerty','qwerty'),(2,'Juan Perez','JuanPerez@hotmail.com','JP','JP'),(3,'Jesus Maximiliano Tostado Navarro','pruebaCorreo@gmail.com','JM_tost','prueba1231456_'),(4,'ojaoijdfdsjfd','perezJ@gmail.com','Perez_j','465sdf_'),(5,'aakjhdajskdh ajkhsjdak dh','akjshdaksj@gmail.com','prueba_k','12543');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -172,4 +174,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-08 20:09:31
+-- Dump completed on 2022-05-10 15:50:00
